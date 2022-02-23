b7247e0574d20acaa18f8a03370a6464d077f59a
Daniel <danielbb2000@gmail.com>
2022-02-23 07:32:12 -0300

    feat: add button to add new contact

--- frontend/src/Pages/Contacts.jsx
+++ frontend/src/Pages/Contacts.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import plus from "../Images/plus.svg";
 
 const HomeComponent = styled.div`
   height: 100vh;
@@ -22,6 +23,26 @@ const ContactListWrapper = styled.div`
   border-radius: 50px;
 `;
 
+const ButtonWrapper = styled.button`
+  width: 200px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background-color: #24a0ed;
+  color: white;
+  border-radius: 25px;
+  cursor: pointer;
+  margin-bottom: 25px;
+`;
+
+const PlusWrapper = styled.img`
+  width: 20px;
+  height: 30px;
+  margin-right: 10px; 
+`;
+
 const Contacts = () => {
   return (
     <>
@@ -30,9 +51,11 @@ const Contacts = () => {
           Olá nome, seja bem vindo, abaixo estão todos os seus contatos
           cadastrados
         </ContactMensage>
-        <ContactListWrapper>
-
-        </ContactListWrapper>
+        <ButtonWrapper>
+          <PlusWrapper src={plus} />
+          Adcionar contato
+        </ButtonWrapper>
+        <ContactListWrapper></ContactListWrapper>
       </HomeComponent>
     </>
   );
